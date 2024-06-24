8098c4641a9907dbabe722df42b6e7e97aa0134a
Anshjatana <anshjatana1111@gmail.com>
2024-06-24 14:59:51 +0530

    Fixed background color of animated lists

--- src/components/FollowersComponent.tsx
+++ src/components/FollowersComponent.tsx
@@ -24,9 +24,9 @@ const Notification: React.FC<FollowerData> = ({ login, avatar_url, html_url }) =
         // animation styles
         "transition-all duration-200 ease-in-out hover:scale-[103%]",
         // light styles
-        "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
+        "bg-[var(--color-bg-secondary)] [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
         // dark styles
-        "transform-gpu dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+        "transform-gpu dark:bg-[var(--color-bg-secondary)] dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]"
       )}
     >
       <div className="flex flex-row items-center gap-3">
@@ -43,7 +43,7 @@ const Notification: React.FC<FollowerData> = ({ login, avatar_url, html_url }) =
             <Typography sx={{ fontSize: '16px' }}>@{login}</Typography>
           </figcaption>
           <Link href={html_url} target="_blank">
-            <Typography sx={{ fontSize: '12px'}}>{html_url}</Typography>
+            <Typography sx={{ fontSize: '12px' }}>{html_url}</Typography>
           </Link>
         </div>
       </div>
@@ -52,8 +52,8 @@ const Notification: React.FC<FollowerData> = ({ login, avatar_url, html_url }) =
 };
 
 export default function AnimatedListDemo({ followersData }: FollowersComponentProps) {
-   // Showing only the 10 most recent followers
-   const recentFollowers = followersData.slice(-10);
+  // Showing only the 10 most recent followers
+  const recentFollowers = followersData.slice(-10);
   return (
     <Box className="relative bg-[var(--color-bg-primary)] overflow-auto pb-2 rounded-[2rem] [box-shadow:0_0.25rem_0.5rem_rgba(0,_0,_0,_0.15)] p-4 md:max-w-[400px] w-full h-[500px] md:h-[378px] mx-auto sm:mx-0 max-w-[500px] sm:w-full my-4">
       <Typography className="text-lg mb-2 font-semibold">Recent Followers</Typography>
